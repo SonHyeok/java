5a909155de3df949a39ab9324131e54f94ed2cc5
son <thsgur1212@naver.com>
2023-12-16 16:51:26 +0900

    완성본

--- src/AdminMemberAdd.java
+++ src/AdminMemberAdd.java
@@ -46,44 +46,98 @@ public class AdminMemberAdd extends JFrame {
         setLocationRelativeTo(null);
     }
 
-    // 회원 추가 메소드
+    //회원 추가 메소드
     private void MemberAdd() {
         String email = JOptionPane.showInputDialog("회원 이메일을 입력하세요:");
-        if (email != null && !email.isEmpty()) {
-            if (checkEmail(email)) { // 이미 존재하는 이메일인지 확인
-                JOptionPane.showMessageDialog(null, "이미 존재하는 회원입니다.");
+        if (checkEmail(email)) { // 이미 존재하는 이메일인지 확인
+            JOptionPane.showMessageDialog(null, "이미 존재하는 회원입니다.");
+            return;
+        } else {
+            if (email == null || email.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "잘못된 값 혹은 취소로 회원추가를 종료합니다.");
                 return;
             }
-            // 사용자로부터 회원 정보 입력 받기
-            String password = JOptionPane.showInputDialog("비밀번호를 입력하세요:");
-            String name = JOptionPane.showInputDialog("이름을 입력하세요:");
-            int age = Integer.parseInt(JOptionPane.showInputDialog("나이를 입력하세요:"));
-            String sex = JOptionPane.showInputDialog("성별을 입력하세요:");
-            String phone = JOptionPane.showInputDialog("전화번호를 입력하세요:");
-            Date birth = Date.valueOf(JOptionPane.showInputDialog("생년월일을 입력하세요 (yyyy-mm-dd):"));
+        }
 
-            try {
-                // SQL 쿼리를 사용하여 회원 추가
-                String sql = "INSERT INTO members (MemberEmail, MemberPassword, MemberName, MemberAge, MemberSex, MemberPN, MemberBirth, IsAdmin) " +
-                        "VALUES (?, ?, ?, ?, ?, ?, ?, false)";
-                PreparedStatement statement = connection.prepareStatement(sql);
-                statement.setString(1, email);
-                statement.setString(2, password);
-                statement.setString(3, name);
-                statement.setInt(4, age);
-                statement.setString(5, sex);
-                statement.setString(6, phone);
-                statement.setDate(7, birth);
+        // 비밀번호 입력 받기
+        String password = JOptionPane.showInputDialog("비밀번호를 입력하세요:");
+        if (password == null || password.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "잘못된 값 혹은 취소로 회원추가를 종료합니다.");
+            return;
+        }
 
-                int rowsInserted = statement.executeUpdate();
-                if (rowsInserted > 0) {
-                    JOptionPane.showMessageDialog(null, "회원 추가가 완료되었습니다.");
-                } else {
-                    JOptionPane.showMessageDialog(null, "회원 추가에 실패했습니다.");
-                }
-            } catch (SQLException ex) {
-                ex.printStackTrace();
+        // 이름 입력 받기
+        String name = JOptionPane.showInputDialog("이름을 입력하세요:");
+        if (name == null || name.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "잘못된 값 혹은 취소로 회원추가를 종료합니다.");
+            return;
+        }
+
+        // 나이 입력 받기
+        String ageStr = JOptionPane.showInputDialog("나이를 입력하세요:");
+        if (ageStr == null || ageStr.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "잘못된 값 혹은 취소로 회원추가를 종료합니다.");
+            return;
+        }
+
+        int age;
+        try {
+            age = Integer.parseInt(ageStr);
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(null, "잘못된 값 혹은 취소로 회원추가를 종료합니다.");
+            return;
+        }
+
+        // 성별 입력 받기
+        String sex = JOptionPane.showInputDialog("성별을 입력하세요 (남 or 여):");
+        if (sex == null || sex.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "잘못된 값 혹은 취소로 회원추가를 종료합니다.");
+            return;
+        }
+
+        // 전화번호 입력 받기
+        String phone = JOptionPane.showInputDialog("전화번호를 입력하세요 (010-0000-0000):");
+        if (phone == null || phone.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "잘못된 값 혹은 취소로 회원추가를 종료합니다.");
+            return;
+        }
+
+        // 생년월일 입력 받기
+        String birthStr = JOptionPane.showInputDialog("생년월일을 입력하세요 (yyyy-mm-dd):");
+        if (birthStr == null || birthStr.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "잘못된 값 혹은 취소로 회원추가를 종료합니다.");
+            return;
+        }
+
+        Date birth;
+        try {
+            birth = Date.valueOf(birthStr);
+        } catch (IllegalArgumentException ex) {
+            JOptionPane.showMessageDialog(null, "올바른 생년월일 형식으로 입력하세요.");
+            return;
+        }
+
+        try {
+            // SQL 쿼리를 사용하여 회원 추가
+            String sql = "INSERT INTO members (MemberEmail, MemberPassword, MemberName, MemberAge, MemberSex, MemberPN, MemberBirth, IsAdmin) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, false)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, email);
+            statement.setString(2, password);
+            statement.setString(3, name);
+            statement.setInt(4, age);
+            statement.setString(5, sex);
+            statement.setString(6, phone);
+            statement.setDate(7, birth);
+
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                JOptionPane.showMessageDialog(null, "회원 추가가 완료되었습니다.");
+            } else {
+                JOptionPane.showMessageDialog(null, "회원 추가에 실패했습니다.");
             }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 
